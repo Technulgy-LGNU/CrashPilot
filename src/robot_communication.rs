7a13d053d720fa826b220c3f1858285d29c9898e
Elias Braun <braunelias@tghd.email>
2026-04-04 17:10:58 +0200

    robot sender finished

--- src/robot_communication.rs
+++ src/robot_communication.rs
@@ -1,11 +1,22 @@
 use std::collections::HashMap;
-use std::net::Ipv4Addr;
+use prost::Message;
 use tokio::net::UdpSocket;
-use crate::proto;
+use crate::config;
+use crate::proto::CpRobot;
 
 mod robot_sender;
 mod robot_receiver;
 
-pub async fn send_to_all_robots(socket: &UdpSocket, data: HashMap<Ipv4Addr, proto::CpRobot>, packet_id: u32) {
+pub async fn send_to_all_robots(socket: &UdpSocket, data: &HashMap<u32, CpRobot>, cfg: &config::Config) {
+  let mut buf = vec![0u8; 1024];
+  for robot in data.keys() {
+    let robot_data = data.get(robot).unwrap();
+    robot_data.encode(&mut buf).unwrap();
 
+    let robot_addr = format!("{}:{}", cfg.robots.get(robot).unwrap().ip, 1024);
+    match socket.send_to(&buf, robot_addr).await {
+      Ok(_) => println!("Sent data to robot {}", robot),
+      Err(e) => eprintln!("Failed to send data to robot {}: {}", robot, e),
+    };
+  }
 }
