925810c4f89647a47c8b60769162f542021a97e2
Elias Braun <braunelias@tghd.email>
2026-04-04 19:10:45 +0200

    improved error handling and messages

--- src/ssl_communication.rs
+++ src/ssl_communication.rs
@@ -8,16 +8,22 @@ use crate::ssl_communication::udp_listener::spawn_udp_listener;
 pub mod udp_listener;
 pub mod create_multicast_socket;
 
-pub async fn get_ssl_data<'a>(cfg: &Config, tx: mpsc::Sender<Event>){
+pub async fn get_ssl_data(cfg: &Config, tx: mpsc::Sender<Event>){
   // Referee
-  let ref_socket = create_multicast_socket(cfg.ssl.ssl_gc_ip, cfg.ssl.ssl_gc_port);
+  let ref_socket = match create_multicast_socket(cfg.ssl.ssl_gc_ip, cfg.ssl.ssl_gc_port) {
+    Ok(s) => s,
+    Err(e) => panic!("Failed to create multicast socket for referee: {}", e),
+  };
 
-  spawn_udp_listener::<Referee>(ref_socket.unwrap(), tx.clone(), Event::Referee);
+  spawn_udp_listener::<Referee>(ref_socket, tx.clone(), Event::Referee);
 
   // Vision
-  let vis_socket = create_multicast_socket(cfg.ssl.ssl_vision_ip, cfg.ssl.ssl_vision_port);
+  let vis_socket = match create_multicast_socket(cfg.ssl.ssl_vision_ip, cfg.ssl.ssl_vision_port) {
+    Ok(s) => s,
+    Err(e) => panic!("Failed to create multicast socket for referee: {}", e),
+  };
 
-  spawn_udp_listener::<TrackerWrapperPacket>(vis_socket.unwrap(), tx.clone(), Event::SslWrapper);
+  spawn_udp_listener::<TrackerWrapperPacket>(vis_socket, tx.clone(), Event::SslWrapper);
 
   // Drop extra sender on stream drop
   drop(tx);
