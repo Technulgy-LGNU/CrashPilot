c1da135bdd14d7c381b78b9d230a47f85362a8b6
Shark <sharktheone@proton.me>
2026-06-13 20:18:51 +0200

    add heads

--- crates/artificial_incompetence/src/modules/nn.rs
+++ crates/artificial_incompetence/src/modules/nn.rs
@@ -2,3 +2,4 @@ mod multi_head_attention;
 mod cross_attention;
 mod transformer;
 mod feed_forward;
+mod heads;
---
+++ crates/artificial_incompetence/src/modules/nn/heads.rs
@@ -0,0 +1,51 @@
+use tch::{nn, Tensor};
+
+pub struct PointerHead {
+    query_proj: nn::Linear,
+}
+
+impl PointerHead {
+    pub fn new(vs: &nn::Path, in_dim: i64, key_dim: i64) -> Self {
+        let query_proj = nn::linear(vs / "query_proj", in_dim, key_dim, Default::default());
+
+        Self { query_proj }
+    }
+
+    pub fn forward(&self, query: &Tensor, keys: &Tensor) -> Tensor {
+        query.apply(&self.query_proj).matmul(&keys.transpose(-1, -2))
+    }
+}
+
+pub struct CategoricalHead {
+    linear: nn::Linear,
+}
+
+impl CategoricalHead {
+    pub fn new(vs: &nn::Path, in_dim: i64, out_dim: i64) -> Self {
+        let linear = nn::linear(vs / "linear", in_dim, out_dim, Default::default());
+
+        Self { linear }
+    }
+
+    pub fn forward(&self, xs: &Tensor) -> Tensor {
+        xs.apply(&self.linear)
+    }
+}
+
+pub struct ValueHead {
+    l1: nn::Linear,
+    l2: nn::Linear,
+}
+
+impl ValueHead {
+    pub fn new(vs: &nn::Path, in_dim: i64, hidden_dim: i64) -> Self {
+        let l1 = nn::linear(vs / "l1", in_dim, hidden_dim, Default::default());
+        let l2 = nn::linear(vs / "l2", hidden_dim, 1, Default::default());
+
+        Self { l1, l2 }
+    }
+
+    pub fn forward(&self, xs: &Tensor) -> Tensor {
+        xs.apply(&self.l1).gelu("none").apply(&self.l2)
+    }
+}
